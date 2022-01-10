70b730b9f208ab2a24c81dcfdeb2c2a4bd655d1f
Marcos Bracco <mb@sweetdreams3d.com>
2022-01-10 15:04:04 -0300

    updating csharp to cpp tests

---
+++ test/csharp-2-cpp/build/idl.bat
@@ -0,0 +1,1 @@
+idl_compiler.exe ..\message_one.idl ..\gen_message_one.h -csharp=..\gen_message_one.cs -m=mtest
--- test/message/build/idl.bat
+++
@@ -1,1 +0,0 @@
-idl_compiler.exe ..\message_one.idl ..\gen_message_one.h -csharp=..\gen_message_one.cs -m=mtest
